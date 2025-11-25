a6c7597551146a5c55c232d0af8aa08eec2b0e1f
felipelundgren-bsatech <felipec5l2@hotmail.com>
2025-11-25 14:47:17 -0300

    feat: win/lose anim minebuttion

--- Assets/Scripts/Prefabs/MineButtonBehavior.cs
+++ Assets/Scripts/Prefabs/MineButtonBehavior.cs
@@ -1,11 +1,32 @@
+using System;
 using UnityEngine;
 
 public class MineButtonBehavior : MonoBehaviour
 {
     private Animator animator;
+    
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     public void OnClickWinOrLose()
     {
-        animator.SetTrigger("win");
+        PlayLoseAnimation();
+    }
+
+
+
+    private void PlayLoseAnimation()
+    {
+        animator.SetTrigger("Lose");
+       
+    }
+
+    private void PlayWinAnimation()
+    {
+        animator.SetTrigger("Win");
     }
     
 }
