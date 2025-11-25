9263329ea65fd47c7fe10871a44773b5d53ac615
felipelundgren-bsatech <felipec5l2@hotmail.com>
2025-11-25 15:49:00 -0300

    feat: updadte animLose

--- Assets/Scripts/Prefabs/MineButtonBehavior.cs
+++ Assets/Scripts/Prefabs/MineButtonBehavior.cs
@@ -1,1 +1,49 @@
+using System;
+using UnityEngine;
 
+public class MineButtonBehavior : MonoBehaviour
+{
+    private bool active = false;
+    private Animator animator;
+    [SerializeField] private ParticleSystem hitParticle;
+    [SerializeField] private GameObject bombImage;
+    [SerializeField] private GameObject coinImage;
+
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void OnClickWinOrLose()
+    {
+        if (active)
+        {
+            PlayWinAnimation();
+        }
+        
+        
+    }
+
+
+
+    private void PlayLoseAnimation()
+    {
+        animator.SetTrigger("Lose");
+        PlayHitParticle();
+        bombImage.SetActive(true);
+
+    }
+
+    private void PlayWinAnimation()
+    {
+        animator.SetTrigger("Win");
+        coinImage.SetActive(true);
+    }
+    
+    private void PlayHitParticle()
+    {
+        ParticleSystem instantiatedParticle = Instantiate(hitParticle, transform.position, transform.rotation);
+        instantiatedParticle.Play();
+    }
+}
