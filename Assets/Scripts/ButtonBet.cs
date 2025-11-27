cad8601c6ee74abee7d7752df0c7e7baef545ccb
raulbsa <raulvictor1919@outlook.com>
2025-11-27 17:27:08 -0300

    Script Bet Button

--- Assets/Scripts/ButtonBet.cs
+++ Assets/Scripts/ButtonBet.cs
@@ -5,60 +5,94 @@ using UnityEngine.UI;
 public class ButtonBet : MonoBehaviour
 {
     public Button betButton;
+    public Button mineButton;
+
     public TextMeshProUGUI buttonBetText;
     private string betText = "BET";
     private bool onGaming = false;
     private bool cashout = false;
+    private bool mineActive = false;
 
     public Image buttonBetLaranja;
     public Image buttonBetVermelho;
-
     private Image buttonBetAtual;
+
+    public Image buttonMineRosa;
+    public Image buttonMineAmarelo;
+    private Image buttonMineAtual;
 
     void Start()
     {
         buttonBetAtual = buttonBetLaranja;
-        UpdateButtonState();
-    }
+        buttonMineAtual = buttonMineRosa;
 
-    void Update()
-    {
-        buttonBetText.text = betText;
-        betButton.image.sprite = buttonBetAtual.sprite;
+        UpdateButtonState();
     }
 
     void UpdateButtonState()
     {
         // limpa listeners antigos
         betButton.onClick.RemoveAllListeners();
+        mineButton.onClick.RemoveAllListeners();
 
-        if (!onGaming && !cashout)
+        if (!onGaming)
         {
             betButton.onClick.AddListener(IsGaming);
         }
-        else if (onGaming && cashout)
+        else if (onGaming && !mineActive)
         {
-            betButton.onClick.AddListener(EndGame);
+            mineButton.onClick.AddListener(MineTeste);
+        }
+        //else if (onGaming && mineActive)
+        //{
+        //    betButton.onClick.AddListener(PossibleCashout);
+        //}
+        else if (onGaming && mineActive)
+        {
+            //cashout = true;
+            betButton.onClick.AddListener(PossibleCashout);
         }
     }
 
     void IsGaming()
     {
-        betText = "CASHOUT\n4,00 BRL";
+        buttonBetText.text = "CASHOUT\n4,00 BRL";
         onGaming = true;
-        cashout = true;
-        buttonBetAtual = buttonBetVermelho;
+        SetButtonAlpha(betButton, 0.5f);
+        betButton.image.sprite = buttonBetVermelho.sprite;
 
         UpdateButtonState();
     }
 
-    void EndGame()
+
+    void PossibleCashout()
     {
-        betText = "BET";
+        buttonBetText.text = "BET";
         onGaming = false;
+        mineActive = false;
         cashout = false;
-        buttonBetAtual = buttonBetLaranja;
+        betButton.image.sprite = buttonBetLaranja.sprite;
+        mineButton.image.sprite = buttonMineRosa.sprite;
 
         UpdateButtonState();
     }
+
+    void SetButtonAlpha(Button button, float alpha)
+    {
+        Color c = button.image.color;
+        c.a = alpha;
+        button.image.color = c;
+    }
+
+    void MineTeste()
+    {
+        cashout = true;
+        SetButtonAlpha(betButton, 1.0f);
+
+        mineActive = true;
+        mineButton.image.sprite = buttonMineAmarelo.sprite;
+        UpdateButtonState();
+    }
+
+
 }
