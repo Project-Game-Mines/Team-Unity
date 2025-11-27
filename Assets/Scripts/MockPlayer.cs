441d5664491764e6ded67e7e9ba25bc5513c69bd
felipelundgren-bsatech <felipec5l2@hotmail.com>
2025-11-27 17:58:11 -0300

    feat: fixed commit mockPlayer

--- Assets/Scripts/MockPlayer.cs
+++ Assets/Scripts/MockPlayer.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class MockPlayer : MonoBehaviour
 {
     [SerializeField] private GameManager gameManager;
+    
     public int playerid = 123;
     public float balance = 5000.45f;
     
