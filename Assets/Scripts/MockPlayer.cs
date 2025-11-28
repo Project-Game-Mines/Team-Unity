b6e2a324c22f7d5be0b5e346032e6a773b5d3aca
felipelundgren-bsatech <felipec5l2@hotmail.com>
2025-11-28 14:42:32 -0300

    feat: Creating a randomList

--- Assets/Scripts/MockPlayer.cs
+++ Assets/Scripts/MockPlayer.cs
@@ -1,4 +1,5 @@
 
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MockPlayer : MonoBehaviour
@@ -7,6 +8,21 @@ public class MockPlayer : MonoBehaviour
     
     public int playerid = 123;
     public float balance = 5000.45f;
-    
-    
+    public List<int> mineList = new();
+
+
+    void Start()
+    {
+        CratingRandomMines();
+    }
+    public void CratingRandomMines()
+    {
+        const int limiteMaximoExclusivo = 25;
+        for (int i = 0; i < 3; i++)
+        {
+            int numeroAleatorio = Random.Range(0, limiteMaximoExclusivo);
+            mineList.Add(numeroAleatorio);
+        }
+    }
+
 }
