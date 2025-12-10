46f4d413c99201b50311ca3ee2b5e48370c67d83
raulbsa <raulvictor1919@outlook.com>
2025-12-10 16:15:46 -0300

    feat: farol logo

--- Assets/Scripts/UI/Lights.cs
+++ Assets/Scripts/UI/Lights.cs
@@ -6,10 +6,13 @@ public class Lights : MonoBehaviour
     public GameObject light1;
     public GameObject light2;
     public GameObject light3;
+    public GameObject farol1;
+    public GameObject farol2;
 
     void Start()
     {
         StartCoroutine(LightsSequenceLoop());
+        StartCoroutine(LightsFarolLoop());
     }
 
     private IEnumerator LightsSequenceLoop()
@@ -34,6 +37,23 @@ public class Lights : MonoBehaviour
             light3.SetActive(true);
             yield return wait;
             
+        }
+    }
+    
+    private IEnumerator LightsFarolLoop()
+    {
+        // Cache de WaitForSeconds para evitar alocações contínuas
+        WaitForSeconds wait = new WaitForSeconds(0.6f);
+
+        while (true) // repete para sempre
+        {
+            farol1.SetActive(true);
+            farol2.SetActive(false);
+            yield return wait;
+
+            farol1.SetActive(false);
+            farol2.SetActive(true);
+            yield return wait;
         }
     }
 }
